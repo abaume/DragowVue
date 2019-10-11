0c32ef2f03e0c296226a9066d7f0c51b38da3a78
Aurore <aurore.baume@dotsafe.fr>
2019-10-11 22:58:14 +0200

    create files and routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,11 @@ import AddAppearance from '@/components/pages/administration/AddAppearance'
 import AddRace from '@/components/pages/administration/AddRace'
 import NotFound from '@/components/pages/NotFound'
 import Index from '@/components/pages/Index'
+import Login from '@/components/pages/authentication/Login'
+import Register from '@/components/pages/authentication/Register'
 
-Vue.use(Router)
-Vue.use(Vuetify)
+Vue.use(Router);
+Vue.use(Vuetify);
 
 export default new Router({
   routes: [
@@ -31,6 +33,16 @@ export default new Router({
       path: '/ajouter-race',
       name: 'race-create',
       component: AddRace
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     },
     {
       path: '*', component: NotFound
